66e26b5d28d840a7122491c56bc9dc8e6d2b4471
Sugeiri <danielacastilko@gmail.com>
2025-10-15 01:32:58 -0600

    correccion firebase

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,12 +1,17 @@
 // lib/firebase-admin.ts
-import { getApps, initializeApp, cert, type App } from 'firebase-admin/app';
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const app: App =
-  getApps().length
-    ? getApps()[0]!
-    : initializeApp({
-        credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!)),
-      });
+const sa = process.env.FIREBASE_SERVICE_ACCOUNT as string; 
+
+if (!sa) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT not set');
+}
+
+const app = getApps().length
+  ? getApps()[0]
+  : initializeApp({
+      credential: cert(JSON.parse(sa)),
+    });
 
 export const db = getFirestore(app);
